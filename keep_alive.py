ba77f9a2a1e25313ec24cdf83b80820d0b0516be
rjm2007 <rudrakshmehta896@gmail.com>
2026-04-03 00:41:37 +0530

    changes

--- keep_alive.py
+++ keep_alive.py
@@ -15,7 +15,7 @@ import os
 import time
 import urllib.request
 
-RENDER_URL = os.getenv("RENDER_URL", "https://vapi-deployed.onrender.com")
+RENDER_URL = os.getenv("RENDER_URL", "https://vapi-deployed-1.onrender.com")
 INTERVAL = 10 * 60  # 10 minutes
 
 def ping():
